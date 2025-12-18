2e3afb7172849ccb6be4e2b0f600b4e7c5ff01c1
GDtharusha <iqoption298@gmail.com>
2025-12-19 02:47:12 +0530

    U8-Fix

--- android/app/src/main/java/com/keybord/app/MainActivity.java
+++ android/app/src/main/java/com/keybord/app/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends BridgeActivity {
     }
     
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         // Re-notify when app comes back to foreground
         handler.postDelayed(() -> {
